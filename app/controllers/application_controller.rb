80d10f4af060f149e5cb98df866d0d57ef7245a4
Cosimo Ranieri <engagedev1@gmail.com>
2016-09-24 01:59:53 +0200

    add csrf unless json request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  protect_from_forgery unless: -> { request.format.json? }
 
   def payment_api
     CoolpayApi.new(ApiAdaptor.new(PaymentService::CONFIG["payment_api"]["url"]))
